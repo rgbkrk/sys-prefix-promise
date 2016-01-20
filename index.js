788c423f90dc11f222ee81b7c6a942a6cbe91d12
Kyle Kelley <rgbkrk@gmail.com>
2016-01-20 12:59:27 -0600

    Fully support old node.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var exec = require('child_process').exec;
 module.exports = function sysPrefix() {
   return new Promise(function(resolve, reject) {
     exec('python -c \'import sys; print(sys.prefix)\'',
-      (err, stdout) => {
+      function(err, stdout) {
         if (err !== null) {
           reject(err);
         }
